1385c59a72fec53f66c9550f309ddf87a216de80
Rahim Atabaev <119874001+Rahimred@users.noreply.github.com>
2023-03-04 17:15:42 +0600

    add server

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
+import controlwork.CalendarModel;
+import controlwork.Control;
+
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    private static CalendarModel calendarModel = new CalendarModel();
+    public static void main(String[] args){
+        try {
+            new Control("localhost", 9889).start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
